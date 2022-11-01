84b83fdd8fe2e68080be0320d61cdcc21c3d74be
ivanchenko.alm@gmail.com <ivanchenko.alm@gmail.com>
2022-11-01 09:55:09 +0300

    Добавьте файлы проекта.

---
+++ Queue.h
@@ -0,0 +1,55 @@
+#pragma once
+
+template <typename T>
+class Queue
+{
+  T* mem;
+  // mem: ..., begin, ..., ..., end, ...
+  size_t sz;
+  int begin; // индекс первого элемента
+  int end; // индекс последнего элемента
+
+public:
+  static const size_t MAX_SIZE = 10000;
+  Queue() {
+    mem = new T[MAX_SIZE];
+    sz = 0;
+    begin = 0;
+    end = -1;
+  }
+  
+  size_t size() {
+    return sz;
+  }
+  bool empty() {
+    return sz == 0;
+  }
+  bool full() {
+    return sz == MAX_SIZE;
+  }
+
+  void push(const T& x) {
+    if (full()) {
+      throw std::length_error("Queue is full");
+    }
+    end = (end + 1) % MAX_SIZE;
+    mem[end] = x;
+    sz++;
+  }
+  T top() {
+    if (empty()) {
+      throw std::out_of_range("Queue is empty");
+    }
+    return mem[begin];
+  }
+  void pop() {
+    if (empty()) {
+      throw std::out_of_range("Queue is empty");
+    }
+    begin++;
+    sz--;
+  }
+
+
+};
+
---
+++ main.cpp
@@ -0,0 +1,42 @@
+﻿
+#include <iostream>
+#include "Queue.h"
+
+using namespace std;
+
+int main()
+{
+  Queue<int> q1;
+  q1.push(1);
+  q1.push(2);
+  q1.push(7);
+  q1.push(3);
+  // now q1 is 1, 2, 3, 7
+  while (q1.size() > 2) {
+    cout << q1.top() << endl;
+    q1.pop();
+  }
+  // now q1 is 3, 7
+  q1.push(8);
+  q1.push(9);
+  while (!q1.empty()) {
+    cout << q1.top() << endl;
+    q1.pop();
+  }
+  // исключение - pop пустой очереди
+  try {
+    q1.pop();
+  }
+  catch (exception e) {
+    cout << e.what() << endl;
+  }
+  // исключение - push в заполненную очередь
+  try {
+    for (int i = 0; i < q1.MAX_SIZE + 10; i++)
+      q1.push(1);
+  }
+  catch (exception e) {
+    cout << e.what() << endl;
+  }
+  
+}
